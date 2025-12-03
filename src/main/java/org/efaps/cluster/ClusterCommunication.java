377a0d40f7b8f285f9aa328422f7cd4940537da2
Jan Moxter <jan.moxter@moxter.net>
2025-12-03 11:30:21 -0500

    Add jgroups

---
+++ src/main/java/org/efaps/cluster/ClusterCommunication.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright © 2003 - 2024 The eFaps Team (-)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.efaps.cluster;
+
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.Receiver;
+import org.jgroups.View;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClusterCommunication
+{
+
+    private static final Logger LOG = LoggerFactory.getLogger(ClusterCommunication.class);
+
+    private static JChannel CHANNEL;
+
+    public static void initialize()
+    {
+        final var config = ConfigProvider.getConfig();
+        final var fileOpt = config.getOptionalValue("core.jGroups.file", String.class);
+        if (fileOpt.isPresent()) {
+            final var file = fileOpt.get();
+            LOG.info("Got JGroupsFile: {}", file);
+
+            try {
+                final var channel = new JChannel(file);
+                channel.setReceiver(new Receiver()
+                {
+
+                    @Override
+                    public void receive(final Message msg)
+                    {
+                        LOG.info("received JGroups message: {}", msg);
+                    }
+
+                    @Override
+                    public void viewAccepted(final View view)
+                    {
+                        LOG.info("received JGroups view: {}", view);
+                    }
+                });
+
+                final var clusterName = config.getValue("core.jGroups.clusterName", String.class);
+                channel.connect(clusterName);
+
+                CHANNEL = channel;
+            } catch (final Exception e) {
+                LOG.error("Catched", e);
+            }
+        } else {
+            LOG.debug("No JGroupsFile");
+        }
+    }
+
+    public static JChannel getChannel()
+    {
+        return CHANNEL;
+    }
+}
---
+++ src/main/java/org/efaps/cluster/StreamableWrapper.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright © 2003 - 2024 The eFaps Team (-)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.efaps.cluster;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Objects;
+
+import org.efaps.admin.program.esjp.EFapsApplication;
+import org.efaps.admin.program.esjp.EFapsClassLoader;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.jgroups.Global;
+import org.jgroups.util.ByteArray;
+import org.jgroups.util.SizeStreamable;
+import org.jgroups.util.Util;
+
+@EFapsUUID("deceedf1-9dc9-4f8c-9178-0c276a926969")
+@EFapsApplication("eFaps-Kernel")
+public class StreamableWrapper
+    implements SizeStreamable
+{
+
+    protected Object obj;
+    protected ByteArray serialized;
+
+    public StreamableWrapper()
+    {
+    }
+
+    public StreamableWrapper(final Serializable obj)
+    {
+        this.obj = Objects.requireNonNull(obj);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getObject()
+    {
+        return (T) obj;
+    }
+
+    public synchronized StreamableWrapper setObject(final Serializable obj)
+    {
+        this.obj = obj;
+        this.serialized = null;
+        return this;
+    }
+
+    public synchronized ByteArray getSerialized()
+    {
+        if (serialized != null)
+            return serialized;
+        try {
+            return serialized = Util.objectToBuffer(obj);
+        } catch (final Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public int getLength()
+    {
+        return getSerialized().getLength();
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("obj: %s %s", obj, serialized != null ? "(" + serialized.getLength() + " bytes)" : "");
+    }
+
+    @Override
+    public int serializedSize()
+    {
+        final int retval = Global.INT_SIZE; // length (integer)
+        if (obj == null) {
+            return retval;
+        }
+        return retval + getLength();
+    }
+
+    @Override
+    public void writeTo(DataOutput out)
+        throws IOException
+    {
+        if (obj != null) {
+            final ByteArray arr = getSerialized();
+            out.writeInt(arr.getLength());
+            out.write(arr.getArray(), 0, arr.getLength());
+        } else {
+            out.writeInt(-1);
+        }
+    }
+
+    @Override
+    public void readFrom(DataInput in)
+        throws IOException, ClassNotFoundException
+    {
+        final int len = in.readInt();
+        if (len == -1) {
+            return;
+        }
+        final byte[] tmp = new byte[len];
+        in.readFully(tmp, 0, len);
+        serialized = new ByteArray(tmp);
+        obj = Util.objectFromBuffer(serialized, EFapsClassLoader.getInstance());
+    }
+}
